1a76272d1f1782a1cf944fac3ae65da7ddc4f142
Wolfram Arnold <wolfram@rubyfocus.biz>
2012-04-20 13:14:43 -0700

    Configure production FB app for Omniauth, so that redirect to Heroku app is allowed.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -57,6 +57,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
                 read_stream]
 
 
+  # Use Heroku-configured APP ID and SECRET for production (pointing to Social Islands)
+  # or use Social Islands Dev app locally. This matters for redirect URL.
+  fb_app_id      = Rails.env.production? ? ENV['FACEBOOK_APP_ID'] : '398796910145811'
+  fb_app_secret  = Rails.env.production? ? ENV['FACEBOOK_SECRET'] : '71e24820b766e844985e9ff92e1ba119'
+
   provider :facebook, '398796910145811', '71e24820b766e844985e9ff92e1ba119',
            scope: fb_perms.join(',')
 
