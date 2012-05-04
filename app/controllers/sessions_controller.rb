30c3a8f53fb01b7cc3bbcdd6d7cb6d758895328f
Wolfram Arnold <wolfram@rubyfocus.biz>
2012-05-03 20:40:37 -0700

    Store token expiration in database. Also, secret is not populated in Facebook OAuth, so don't make it a required field. Configure mongoid yaml for Heroku. Remove MongoMapper config.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,17 +12,15 @@ class SessionsController < ApplicationController
       user = User.where(provider: omni['provider'], uid: omni['uid']).first
       if user.nil?
         user = User.new(provider: omni['provider'], uid: omni['uid'], image: omni['info']['image'], name: omni['info']['name'])
-        user.token = omni['credentials']['token']
-        user.secret = omni['credentials']['secret']
+        set_credentials(user,omni['credentials'])
         user.save!
       else
         # TODO: Possibly not a good idea to store the token and secret here -- is this vulnerable?
-        user.token = omni['credentials']['token']
-        user.secret = omni['credentials']['secret']
+        set_credentials(user,omni['credentials'])
         user.save!
       end
 
-      # TODO: Secure this with fingerprint or use Devise
+      # TODO: Is the session automatically secure with a fingerprint digest? I think so, but need to double check
       session[:user_id] = user.to_param
       redirect_to send("#{omni['provider']}_profile_path")
     end
@@ -36,6 +34,15 @@ class SessionsController < ApplicationController
   def destroy
     reset_session
     redirect_to root_path
+  end
+
+  private
+
+  def set_credentials(user,omni_credentials)
+    user.token = omni_credentials['token']
+    user.secret = omni_credentials['secret']
+    user.expires_at = Time.at(omni_credentials['expires_at'])
+    user.expires = omni_credentials['expires']
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,20 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :uid,      type: String
-  field :provider, type: String
-  field :image,    type: String
-  field :name,     type: String
-  field :token,    type: String
-  field :secret,   type: String
+  field :uid,        type: String
+  field :provider,   type: String
+  field :image,      type: String
+  field :name,       type: String
+  field :token,      type: String
+  field :secret,     type: String
+  field :expires_at, type: DateTime
+  field :expires,    type: Boolean
 
   index [:uid, :provider], unique: true
 
   attr_accessible :uid, :provider, :image, :name
 
-  validates :uid, :provider, :image, :name, :token, :secret, presence: true
+  validates :uid, :provider, :image, :name, :token, presence: true
 
   has_one :facebook_profile, dependent: :nullify
 
