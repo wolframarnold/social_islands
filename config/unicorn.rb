ec82ad008ca104b8d5b67789e0a26155e78200b3
Wolfram Arnold <wolfram@rubyfocus.biz>
2012-05-08 11:50:18 -0700

    Fix unicorn worker to be "verbose" -- i.e. print output to log

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
-worker_processes 3
+worker_processes 2
 timeout 30
 
 @resque_pid = nil
 
 before_fork do |server, worker|
-  @resque_pid ||= spawn("bundle exec rake resque:work QUEUES=fb_fetcher")
+  @resque_pid ||= spawn("bundle exec rake resque:work VERBOSE=1 QUEUES=fb_fetcher")
 end
