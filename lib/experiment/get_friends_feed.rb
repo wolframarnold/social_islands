6e4897454aa3a2eff34713c1ec99476e99ff4533
Weidong Yang <wdyangmail@yahoo.com>
2012-06-29 11:38:01 -0700

    Pull feeds for all the friends for user with name, name. (have to save in the end, due to mongodb failure for subsequent saving) Processing feed into directional relationships. on-going

---
+++ lib/experiment/get_friends_feed.rb
@@ -0,0 +1,16 @@
+
+def get_and_save_friends_feed(name)
+  usr=User.where(name:name).first
+  fb=FacebookProfile.unscoped.where(user_id:usr._id).first
+  @graph=Koala::Facebook::API.new(usr.token)
+
+  (0..(friends.count-1)).each do |i1|
+    friend = friends[i1]
+    if friend["feed"].blank?
+      feed=@graph.get_connection(friend["uid"], "feed")
+      friend["feed"]=feed
+    end
+    puts i1.to_s+"  "+(friend["feed"].blank? ? 0 : friend["feed"].count).to_s
+  end
+  fb.save!
+end
