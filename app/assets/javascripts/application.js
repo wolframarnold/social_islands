611c2a819097bb9bcca37d632fb497de8e34e6c6
Wolfram Arnold <wolfram@rubyfocus.biz>
2013-04-08 09:31:05 -0700

    Re-add sigma.js to asset pipeline. App on Heroku was failing b/c sigma.js was excluded from asset pipeline.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require sigma.js
 //= require social_islands
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ SocialIslands::Application.configure do
 
 
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Expands the lines which load the assets
   config.assets.debug = false
